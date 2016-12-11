e91432aca590ced7a617033196c86f55284beb8f
joshliu <61315730@qq.com>
2016-12-11 23:09:06 +0800

    [Config]Add 'spark' mode

--- config.py
+++ config.py
@@ -6,7 +6,4 @@ import os
 pyspark_path = "D:\spark-2.0.0-bin-hadoop2.7\spark-2.0.0-bin-hadoop2.7\python"
 os.sys.path.append(pyspark_path)
 
-use_spark = False
-
-print __package__
-print __name__
+use_spark = False;
