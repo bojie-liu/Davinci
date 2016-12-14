6d788fd8e96f415ec04f4ee9bfffd35889a03bc2
joshliu <joshliu>
2016-12-14 17:30:32 +0800

    [spark]fix "rmse equals to NaN"

--- sparkdemo/sparksqlmain.py
+++ sparkdemo/sparksqlmain.py
@@ -1,22 +1,23 @@
 
 from pyspark import SparkConf
-from pyspark.sql import SparkSession, Row
+from pyspark.sql import SparkSession
 from pyspark.ml import Pipeline
 from pyspark.ml.classification import LogisticRegression
 from pyspark.ml.recommendation import ALS
 from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.sql.functions import isnan
 
 if __name__ == '__main__':
 	conf = SparkConf().setMaster('local')
 	conf.set("spark.sql.warehouse.dir", "E:/spark-warehouse/")
 	spark = SparkSession.builder.appName('2.0 ml demo').config(conf=conf).getOrCreate()
-	df = spark.read.csv(r'E:\git\ml-20m\ml-20m\ratings.csv', header=True, inferSchema=True)
+	df = spark.read.csv(r'E:\working git\ml-20m\ml-20m\ratings.csv', header=True, inferSchema=True)
 	(training, test) = df.randomSplit([0.8, 0.2])
 	als = ALS(maxIter=5, regParam=0.01, userCol="userId", itemCol="movieId", ratingCol="rating")
 	model = als.fit(training)
 
 	predictions = model.transform(test)
-	predictions.show()
+	predictions_withoutNAN = predictions.filter(isnan(predictions.prediction) == False)
 	evaluator = RegressionEvaluator(metricName="rmse", labelCol="rating", predictionCol="prediction")
-	rmse = evaluator.evaluate(predictions)
+	rmse = evaluator.evaluate(predictions_withoutNAN)
 	print("rmse = " + str(rmse))
