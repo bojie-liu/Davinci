95a7c98a68a8dae72e11fb504ceb4bf9a154b21d
joshliu <joshliu>
2016-12-21 20:43:14 +0800

    [spark]word2vec_demo

--- sparkdemo/featureextractor.py
+++ sparkdemo/featureextractor.py
@@ -1,4 +1,4 @@
-from pyspark.ml.feature import HashingTF, IDF, Tokenizer
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer, Word2Vec, ChiSqSelector
 from pyspark import SparkConf
 from pyspark.sql import SparkSession
 
@@ -17,12 +17,31 @@ def TFIDF_demo():
 	idf = IDF(inputCol="rawFeatures", outputCol="features")
 	idfModel = idf.fit(featurizedData)
 	rescaledData = idfModel.transform(featurizedData)
-	for features_label in rescaledData.select("features", "label").take(3):
-	    print(features_label)
 
-def word2vec_demo:
-	
+	selector = ChiSqSelector(numTopFeatures=2, featuresCol="features",
+                         outputCol="selectedFeatures", labelCol="label")
+	selected_result = selector.fit(rescaledData).transform(rescaledData)
+	for features_label in selected_result.select("features", "selectedFeatures").take(3):
+	    print(features_label)
+	# for features_label in rescaledData.select("features", "label").take(3):
+	#     print(features_label)
+
+def word2vec_demo():
+	# Input data: Each row is a bag of words from a sentence or document.
+	documentDF = spark.createDataFrame([
+	    ("Hi I heard about Spark".split(" "), ),
+	    ("I wish Java could use case classes".split(" "), ),
+	    ("Logistic regression models are neat".split(" "), )
+	], ["text"])
+	# Learn a mapping from words to Vectors.
+	word2Vec = Word2Vec(vectorSize=3, minCount=0, inputCol="text", outputCol="result")
+	model = word2Vec.fit(documentDF)
+	result = model.transform(documentDF)
+
+	for feature in result.select("result").take(3):
+	    print(feature)
 
 if __name__ == "__main__":
 	conf = SparkConf().setMaster("local").set("spark.sql.warehouse.dir", "E:/spark-warehouse/")
 	spark = SparkSession.builder.appName('2.0 ml demo').config(conf=conf).getOrCreate()
+	TFIDF_demo()
