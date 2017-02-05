6a3a25c143de51e3b8788e42863866588436d918
joshua-liubojie <a5561379@163.com>
2017-02-05 22:18:39 +0800

    Add logging module

--- utils/log.py
+++ utils/log.py
@@ -1,12 +1,30 @@
+#!/usr/bin/python env
+# -*- coding: utf-8 -*-
+
+"""
+Description:
+"""
+
+__author__ = "joshliu"
+
+import os
+import logging
+import logging.config
 
 
+def get_logger(logger_name):
+	_current_dir = os.path.dirname(os.path.realpath(__file__))
+	logging.config.fileConfig(_current_dir + "/logging.conf")
+	return logging.getLogger(logger_name)
+
+#deprecated
 _verbose = True
-
-def e(msg):
-	print(msg)
-	return
 
 def i(msg):
 	if _verbose == True:
 		print(msg)
 	return
+
+if __name__ == "__main__":
+	logger = get_logger("root")
+	logger.info("hello")
