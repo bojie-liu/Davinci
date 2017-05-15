ae85ecfc20788affcb59a07e7d5af258c20aab92
joshua-liubojie <a5561379@163.com>
2017-05-15 22:42:26 +0800

    Add project config file

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
 #export PYTHONPATH=$PYTHONPATH:"D:\spark-2.0.0-bin-hadoop2.7\spark-2.0.0-bin-hadoop2.7\python"
 
-
 import os
+import platform
 
-pyspark_path = "D:\spark-2.0.0-bin-hadoop2.7\spark-2.0.0-bin-hadoop2.7\python"
+if platform.system() == "Windows":
+    pyspark_path = "D:\spark-2.0.0-bin-hadoop2.7\spark-2.0.0-bin-hadoop2.7\python"
+else:
+    pyspark_path = "/Users/joshualiu/dev/spark-2.0.2-bin-hadoop2.7/python"
+
 os.sys.path.append(pyspark_path)
+
+print(os.sys.path)
 
 mode = "spark"
